ce8754477ce9b2ac5ddd711f5d4ebb17986bbe96
ex0ns <g.tournigand@gmail.com>
2021-06-22 11:41:43 +0200

    Logging

--- src/main/scala/me/ex0ns/inlinexkcd/bot/InlineXKCDBot.scala
+++ src/main/scala/me/ex0ns/inlinexkcd/bot/InlineXKCDBot.scala
@@ -51,9 +51,9 @@ class InlineXKCDBot(val token: String) extends TelegramBot with Commands[Future]
   }
 
   val cronScheduler = Cron4sScheduler.systemDefault[IO]
-  val evenSeconds = Cron.unsafeParse("0 */15 9-23 * ? *")
+  val evenSeconds = Cron.unsafeParse("0 */15 9-23 * * ?")
 
-  val startParse = Stream.eval(IO(parseComic(true)))
+  val startParse = Stream.eval(IO(logger.debug("Calling parse")) *> IO(parseComic(true)))
   val scheduled = cronScheduler.awakeEvery(evenSeconds) >> startParse
 
   scheduled.compile.drain.unsafeRunAndForget()
